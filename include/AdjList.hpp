9c78d44116a6735fbebdde34354c1945c417864f
Northhalf <480563986@qq.com>
2025-05-01 22:16:01 +0800

    将表示节点的数据类型由std::string换为size_t

--- include/AdjList.hpp
+++ include/AdjList.hpp
@@ -7,12 +7,13 @@
 #include <string>
 #include <unordered_map>
 #include <vector>
+typedef size_t VexType;  // 顶点用无符号整数表示
 // 路径类，存储路径长度和依次经过的节点
 class Path {
 public:
-    size_t distance;                      // 路径总距离
-    size_t pass_flow;                     // 路径总人流量
-    std::vector<std::string> pass_nodes;  // 路径经过的节点
+    size_t distance;                 // 路径总距离
+    size_t pass_flow;                // 路径总人流量
+    std::vector<size_t> pass_nodes;  // 路径经过的节点,用节点对应的数字表示
 };
 
 // 临接表边节点
@@ -28,9 +29,9 @@ public:
 
 private:
     std::shared_ptr<EdgeNode> next_edge;  // 指向下一边节点
-    int endvex;     // 有向边指向顶点在临接表顶点数组的下标
-    int pass_flow;  // 人流量
-    int length;     // 边的长度
+    size_t endvex;     // 有向边指向顶点在临接表顶点数组的下标
+    size_t pass_flow;  // 人流量
+    size_t length;     // 边的长度
 };
 
 // 临接表的顶点节点
@@ -43,33 +44,33 @@ public:
     friend class AdjList;
 
 private:
-    std::string vertex;                   // 顶点名字
+    VexType vertex;                       // 顶点对应的数字
     std::shared_ptr<EdgeNode> edge_list;  // 指向边表第一个节点
 };
 
 class AdjList {
 public:
     // 临接表构造函数，传入顶点和边个数
-    AdjList(int vn = 0, int en = 0) : vex_num(vn), edge_num(en) {
+    AdjList(size_t vn = 0, size_t en = 0) : vex_num(vn), edge_num(en) {
         // 提前使数组预留空间
         vexs.reserve(vn);
     }
     // 给临接表插入一条边
     // 传入开始地点和到达地点，路径距离
-    void insert_edge(std::string start, std::string end, int dist);
+    void insert_edge(VexType start, VexType end, size_t dist);
     // Dijkstra最短路径查找，返回得到的路径
-    Path min_dist_Dijkstra(std::string start, std::string end);
+    Path min_dist_Dijkstra(VexType start, VexType end);
     // Floyed算法查找所有最短路径
-    // 返回两个哈希表嵌套，实现像是path["A"]["B"]查找路径
-    std::unordered_map<std::string, std::unordered_map<std::string, Path>>
+    // 返回两个哈希表嵌套，实现像是path[1][2]查找路径
+    std::unordered_map<VexType, std::unordered_map<VexType, Path>>
     min_dist_Floyed();
     // 查找从指定起点和终点的所有可能路径
-    std::vector<Path> find_all_path(std::string start, std::string end);
+    std::vector<Path> find_all_path(VexType start, VexType end);
 
 private:
-    std::vector<VexNode> vexs;                       // 顶点数组
-    std::unordered_map<std::string, int> vex_index;  // 顶点和数组下标的映射
-    int vex_num, edge_num;  // 顶点个数和边的个数
+    std::vector<VexNode> vexs;                      // 顶点数组
+    std::unordered_map<VexType, size_t> vex_index;  // 顶点和数组下标的映射
+    size_t vex_num, edge_num;  // 顶点个数和边的个数
 };
 
 #endif
