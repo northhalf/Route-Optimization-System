a8e68f85532cc087b83156ff9af6bad27228326a
Northhalf <480563986@qq.com>
2025-05-06 19:38:32 +0800

    实现bfs搜寻可能疏散路径

--- include/AdjList.hpp
+++ include/AdjList.hpp
@@ -13,7 +13,7 @@ typedef size_t VexType;  // 顶点用无符号整数表示
 class Path {
 public:
     size_t distance;                 // 路径总距离
-    size_t pass_flow;                // 路径总人流量
+    size_t people_num;               // 路径总人数
     std::vector<size_t> pass_nodes;  // 路径经过的节点,用节点对应的数字表示
 };
 
@@ -23,19 +23,18 @@ public:
     // 构造函数
     // 传入路径长度，人流量，终点位于顶点数组的下标，下一边节点
     EdgeNode(
-        int len = 0, int flow = 0, int end = 0,
+        int len = 0, int people_num = 0, int end = 0,
         std::shared_ptr<EdgeNode> next = nullptr
     )
-        : next_edge(next), endvex(end), pass_flow(flow), length(len) {}
+        : next_edge(next), endvex(end), people_num(people_num), length(len) {}
     // 设置友元类便于访问
     friend class VexNode;
     friend class AdjList;
 
-private:
     std::shared_ptr<EdgeNode> next_edge;  // 指向下一边节点
-    size_t endvex;     // 有向边指向顶点在临接表顶点数组的下标
-    size_t pass_flow;  // 人流量
-    size_t length;     // 边的长度
+    size_t endvex;  // 有向边指向顶点在临接表顶点数组的下标
+    size_t people_num;  // 人数
+    size_t length;      // 边的长度
 };
 
 // 临接表的顶点节点
@@ -44,14 +43,15 @@ public:
     VexNode(VexType vex, std::shared_ptr<EdgeNode> edge_list = nullptr)
         : vertex(vex), edge_list(edge_list) {}
     // 让该顶点增加一条指向另外一个顶点的边，即增加边节点
-    // 传入另外一个顶点在顶点数组的下标，以及人流量和边长度信息
+    // 传入另外一个顶点在顶点数组的下标，以及人数和边长度信息
     // return:
     // 返回是否添加了一条边，如果添加了则返回true，如果边已经存在则返回false
-    bool insert_point_to(size_t end_index, size_t length, size_t pass_flow = 0);
+    bool insert_point_to(
+        size_t end_index, size_t length, size_t people_num = 0
+    );
 
     friend class AdjList;
 
-private:
     VexType vertex;                       // 顶点对应的数字
     std::shared_ptr<EdgeNode> edge_list;  // 指向边表第一个节点
 };
@@ -59,29 +59,45 @@ private:
 class AdjList {
 public:
     // 临接表构造函数，传入顶点和边个数
-    AdjList(size_t vn = 0, size_t en = 0) : vex_num(vn), edge_num(en) {
+    AdjList(size_t vex_num = 0, size_t edge_num = 0)
+        : vex_num(vex_num), edge_num(edge_num) {
         // 提前使数组预留空间
-        vexs.reserve(vn);
+        vexs.reserve(vex_num);
     }
+
     // 给临接表插入一条边
-    // 传入开始地点和到达地点，路径距离，人流量
+    // 传入开始地点和到达地点，路径距离，这条边的人数
     void insert_edge(
-        VexType start, VexType end, size_t length, size_t pass_flow = 0
+        VexType start, VexType end, size_t length, size_t people_num = 0
     );
+
+    // 查找一条可能的疏散路径
+    // @param start, end:       起点和终点
+    // @param max_density:          路人密度的阈值，小于此的路线才可行
+    // @param max_one_path_density:
+    // 最大路径人密度，只有小于此密度的路径才会被考虑
+    // @return 返回一条可行路径
+    Path possible_evacuation(
+        VexType start, VexType end, size_t max_density,
+        size_t max_one_path_density
+    );
+
     // Dijkstra最短路径查找从一个顶点出发的所有最短路径，返回得到的路径
     // 可以使用path[2]得到到某一终点的最短路径
     std::unordered_map<VexType, Path> min_dist_multi_path_Dijkstra(VexType start
     );
+
     // Dijkstra最短路径查找，返回得到的路径
     Path min_dist_one_path_Dijkstra(VexType start, VexType end);
+
     // Floyed算法查找所有最短路径
     // 返回两个哈希表嵌套，实现像是path[1][2]查找路径
     std::unordered_map<VexType, std::unordered_map<VexType, Path>>
     min_dist_Floyed();
+
     // 查找从指定起点和终点的所有可能路径
     std::vector<Path> find_all_path(VexType start, VexType end);
 
-private:
     // 实现对于图的从起点到不同顶点的最短距离数组，对应人流量数组，以及每条最短路径终点的
     // 父节点数组的查找，并将这三个数据以元组形式打包返回
     std::tuple<std::vector<size_t>, std::vector<size_t>, std::vector<size_t>>
--- src/AdjList.cpp
+++ src/AdjList.cpp
@@ -14,14 +14,14 @@
 
 // 考虑可能多次调用，设为inline
 inline bool VexNode::insert_point_to(
-    size_t end_index, size_t length, size_t pass_flow
+    size_t end_index, size_t length, size_t people_num
 ) {
     // edge_node指向边表
     auto edge_node = edge_list;
     // 首先判断边表是否存在
     if (edge_node == NULL) {
         // 如果边表不存在则构造边表
-        edge_list = std::make_shared<EdgeNode>(length, pass_flow, end_index);
+        edge_list = std::make_shared<EdgeNode>(length, people_num, end_index);
         return true;
     }
     // 遍历当前的所有边，直到最后一个节点
@@ -34,13 +34,82 @@ inline bool VexNode::insert_point_to(
     if (edge_node->endvex == end_index) return false;
     // 则这条边为新的边，添加到最后
     edge_node->next_edge =
-        std::make_shared<EdgeNode>(length, pass_flow, end_index);
+        std::make_shared<EdgeNode>(length, people_num, end_index);
     return true;
+}
+
+// 查找可行路径,bfs
+Path AdjList::possible_evacuation(
+    VexType start, VexType end, size_t max_density, size_t max_one_path_density
+) {
+    // 路径节点，记录这个节点的索引，从起点到这个顶点的路径，
+    // 起点到这个节点的长度，以及起点到这个节点的路径人数
+    struct PathNode {
+        size_t index;
+        std::vector<size_t> path;
+        size_t dis;
+        size_t people_num;
+    };
+    // 记录起点和终点在顶点数组的下标
+    size_t start_index = vex_index[start];
+    size_t end_index = vex_index[end];
+    // 队列记录待搜索的顶点
+    // 起点加入队列，无父亲节点，且设置距离和人数都为0
+    std::queue<PathNode> que(std::deque<PathNode>{
+        {start_index, {start_index}, 0, 0}});
+
+    while (!que.empty()) {
+        // 获取队首顶点
+        PathNode node(std::move(que.front()));
+        que.pop();
+        // 遍历与这个顶点相连的边
+        for (auto edge = vexs[node.index].edge_list; edge != nullptr;
+             edge = edge->next_edge) {
+            // 如果这条边的人流密度大于限制密度则不考虑
+            if (edge->people_num / edge->length > max_one_path_density)
+                continue;
+            // 如果这条边的目标地点位于路径中，则跳过，避免重复路径
+            if (std::find(node.path.begin(), node.path.end(), edge->endvex) !=
+                node.path.end()) {
+                continue;
+            }
+
+            // 如果到达了终点，则判断人流量密度是否满足要求，并获取路径信息
+            size_t pp = node.people_num + edge->people_num,
+                   dis = node.dis + edge->length;
+            if (edge->endvex == end_index) {
+                // 如果路径人流量密度大于要求值，则跳过这条路径
+                if (pp / dis > max_density) {
+                    continue;
+                }
+                // 获取路径
+                Path path{dis, pp, std::move(node.path)};
+                // 将终点加入路径
+                path.pass_nodes.emplace_back(end_index);
+                // 将路径中的索引改为顶点
+                std::transform(
+                    path.pass_nodes.begin(), path.pass_nodes.end(),
+                    path.pass_nodes.begin(),
+                    [this](const size_t node_index) {
+                        return this->vexs[node_index].vertex;
+                    }
+                );
+                return path;
+            }
+            // 将这条边到达的顶点视为新顶点，初始化
+            PathNode new_node{edge->endvex, node.path, dis, pp};
+            // 将这个顶点加入路径
+            new_node.path.emplace_back(edge->endvex);
+            // 顶点加入队列中
+            que.push(std::move(new_node));
+        }
+    }
+    return Path{};
 }
 
 // 实现插入一条边
 void AdjList::insert_edge(
-    VexType start, VexType end, size_t length, size_t pass_flow
+    VexType start, VexType end, size_t length, size_t people_num
 ) {
     // 查找起点是否已经处于顶点数组中
     if (vex_index.find(start) == vex_index.end()) {
@@ -62,7 +131,7 @@ void AdjList::insert_edge(
     }
     // 设置起点顶点数据，同时记录是否添加了一条新的边
     bool edge_flag = vexs[vex_index[start]].insert_point_to(
-        vex_index[end], length, pass_flow
+        vex_index[end], length, people_num
     );
     // 如果添加了新边则边数自增1
     if (edge_flag) edge_num++;
@@ -79,8 +148,8 @@ AdjList::__get_dist_passFlow_parent(size_t start_index) {
     // 初始化记录起点到对应顶点的最小距离的dist数组
     std::vector<size_t> dist(vex_num, SIZE_MAX);
     dist[start_index] = 0;  // 起点到自身的距离为0
-    // 记录与dist数组同步的人流量信息
-    std::vector<size_t> pass_flow(vex_num);
+    // 记录与dist数组同步的路径人数信息
+    std::vector<size_t> people_num(vex_num);
 
     // 记录最短路径的每个节点的父亲索引
     std::vector<size_t> parent_index(vex_num, SIZE_MAX);
@@ -115,15 +184,16 @@ AdjList::__get_dist_passFlow_parent(size_t start_index) {
                 now_dist < dist[endvex]) {
                 // 更新最短距离
                 dist[endvex] = now_dist;
-                // 更新对应的人流量信息
-                pass_flow[endvex] = pass_flow[now_vex_index] + edge->pass_flow;
+                // 更新对应的人数信息
+                people_num[endvex] =
+                    people_num[now_vex_index] + edge->people_num;
                 // 更新这个节点的父亲节点
                 parent_index[endvex] = now_vex_index;
                 pq.emplace(endvex, now_dist);
             }
         }
     }
-    return {dist, pass_flow, parent_index};
+    return {dist, people_num, parent_index};
 }
 
 // 查找从指定起点出发的所有最短路径
@@ -138,9 +208,9 @@ std::unordered_map<VexType, Path> AdjList::min_dist_multi_path_Dijkstra(
     size_t start_index = vex_index[start];
 
     // dist:        记录起点到对应顶点的最小距离dist数组
-    // pass_flow:   记录与dist数组同步的人流量信息
+    // people_num:   记录与dist数组同步的人流量信息
     // parend_index:记录最短路径的每个节点的父亲索引
-    auto [dist, pass_flow, parent_index] =
+    auto [dist, people_num, parent_index] =
         __get_dist_passFlow_parent(start_index);
 
     // 记录路径
@@ -150,13 +220,12 @@ std::unordered_map<VexType, Path> AdjList::min_dist_multi_path_Dijkstra(
         // 如果路径不存在则跳过循环
         if (dist[index] == SIZE_MAX) continue;
         // 记录一条路径
-        Path path{dist[index], pass_flow[index], {}};
+        Path path{dist[index], people_num[index], {}};
         // 从终点往回回溯
-        for (size_t now_index = index; now_index != start_index;
+        for (size_t now_index = index; now_index != SIZE_MAX;
              now_index = parent_index[now_index]) {
             path.pass_nodes.emplace_back(vexs[now_index].vertex);
         }
-        path.pass_nodes.emplace_back(start);
         // 将路径逆转，使得起点位于开头
         std::reverse(path.pass_nodes.begin(), path.pass_nodes.end());
         // 将路径加入哈希表中
@@ -188,23 +257,21 @@ Path AdjList::min_dist_one_path_Dijkstra(VexType start, VexType end) {
     size_t end_index = vex_index[end];
 
     // dist:        记录起点到对应顶点的最小距离dist数组
-    // pass_flow:   记录与dist数组同步的人流量信息
+    // people_num:   记录与dist数组同步的人流量信息
     // parend_index:记录最短路径的每个节点的父亲索引
-    auto [dist, pass_flow, parent_index] =
+    auto [dist, people_num, parent_index] =
         __get_dist_passFlow_parent(start_index);
 
     // 定义记录这个顶点是否被搜索过的无序集
     // 判断是否存在路径，不存在则返回空路径
     if (dist[end_index] == SIZE_MAX) return Path{};
     // 记录路径
-    Path path{dist[end_index], pass_flow[end_index], {}};
+    Path path{dist[end_index], people_num[end_index], {}};
     // 从终点往回回溯
-    for (size_t now_index = end_index; now_index != start_index;
+    for (size_t now_index = end_index; now_index != SIZE_MAX;
          now_index = parent_index[now_index]) {
         path.pass_nodes.emplace_back(vexs[now_index].vertex);
     }
-    // 将起点加入到路径中
-    path.pass_nodes.emplace_back(start);
     // 翻转数组，使得起点在开头
     std::reverse(path.pass_nodes.begin(), path.pass_nodes.end());
     return path;
